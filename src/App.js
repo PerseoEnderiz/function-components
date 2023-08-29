89f4ac29db2dbc4929920b604784cbeb5dc1f52e
Perseo Enderiz <perseoheroico@gmail.com>
2023-08-29 11:23:15 -0600

    Envio de datos desde formulario/ validacion de campos con error/Proyectofinal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import FormSignUp from './components/FormSignUp';
 import { Container, Typography } from '@mui/material';
+import { useState } from 'react';
+
 
 function App() {
+  const [datosForm, setDatosForm] = useState([])
+  const manejarDatosForm = (datos) => {
+    setDatosForm([...datosForm,datos])
+  }
+
   return (
     <Container maxWidth="sm" component="Section">
       <Typography variant="h3" align="center"> Formulario </Typography>
-      <FormSignUp/>
+      <FormSignUp manejarDatosForm={manejarDatosForm}/>
     </Container>
   );
 }
--- src/components/FormSignUp/index.jsx
+++ src/components/FormSignUp/index.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+/* import React, { useState } from "react";
 import { TextField, Switch, FormControlLabel, Button, FormGroup } from "@mui/material";
 
-function FormSignUp() {
+function FormSignUp({manejarDatosForm}) {
   const [nombre, setNombre] = useState("");
   const [apellido, setApellido] = useState("");
   const [correo, setCorreo] = useState("");
@@ -28,8 +28,21 @@ function FormSignUp() {
     setPromociones(event.target.checked);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      nombre,
+      apellido,
+      correo,
+      novedades,
+      promociones
+    };
+    console.log(data);
+    manejarDatosForm(data);
+  };
+
   const formulario = (
-    <form>
+    <form onSubmit={handleSubmit}>
       <TextField
         id="nombre"
         label="Nombre"
@@ -64,7 +77,7 @@ function FormSignUp() {
           label="Promociones"
         />
       </FormGroup>
-      <Button variant="contained" color="primary">
+      <Button type="submit" variant="contained" color="primary">
         Registrarse
       </Button>
     </form>
@@ -75,3 +88,119 @@ function FormSignUp() {
 
 export default FormSignUp;
 
+ */
+import React, { useState } from "react";
+import { TextField, Switch, FormControlLabel, Button, FormGroup } from "@mui/material";
+
+function FormSignUp({manejarDatosForm}) {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [novedades, setNovedades] = useState(true);
+  const [promociones, setPromociones] = useState(true);
+  const [nombreError, setNombreError] = useState(false);
+  const [apellidoError, setApellidoError] = useState(false);
+  const [correoError, setCorreoError] = useState(false);
+
+  const handleNombreChange = (event) => {
+    setNombre(event.target.value);
+    if (event.target.value.length < 3) {
+      setNombreError(true);
+    } else {
+      setNombreError(false);
+    }
+  };
+
+  const handleApellidoChange = (event) => {
+    setApellido(event.target.value);
+    if (event.target.value.length < 3) {
+      setApellidoError(true);
+    } else {
+      setApellidoError(false);
+    }
+  };
+
+  const handleCorreoChange = (event) => {
+    setCorreo(event.target.value);
+    if (!event.target.value.includes("@")) {
+      setCorreoError(true);
+    } else {
+      setCorreoError(false);
+    }
+  };
+
+  const handleNovedadesChange = (event) => {
+    setNovedades(event.target.checked);
+  };
+
+  const handlePromocionesChange = (event) => {
+    setPromociones(event.target.checked);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!nombreError && !apellidoError && !correoError) {
+      const data = {
+        nombre,
+        apellido,
+        correo,
+        novedades,
+        promociones
+      };
+      console.log(data);
+      manejarDatosForm(data);
+    }
+  };
+
+  const formulario = (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        id="nombre"
+        label="Nombre"
+        value={nombre}
+        onChange={handleNombreChange}
+        fullWidth
+        margin="normal"
+        error={nombreError}
+        helperText={nombreError ? "El nombre debe tener al menos tres caracteres" : ""}
+      />
+      <TextField
+        id="apellido"
+        label="Apellido"
+        value={apellido}
+        onChange={handleApellidoChange}
+        fullWidth
+        margin="normal"
+        error={apellidoError}
+        helperText={apellidoError ? "El apellido debe tener al menos tres caracteres" : ""}
+      />
+      <TextField
+        id="correo"
+        label="Correo electrónico"
+        value={correo}
+        onChange={handleCorreoChange}
+        fullWidth
+        margin="normal"
+        error={correoError}
+        helperText={correoError ? "El correo electrónico debe contener un @" : ""}
+      />
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={novedades} onChange={handleNovedadesChange} />}
+          label="Novedades"
+        />
+        <FormControlLabel
+          control={<Switch checked={promociones} onChange={handlePromocionesChange} />}
+          label="Promociones"
+        />
+      </FormGroup>
+      <Button type="submit" variant="contained" color="primary">
+        Registrarse
+      </Button>
+    </form>
+  );
+
+  return formulario;
+}
+
+export default FormSignUp;
