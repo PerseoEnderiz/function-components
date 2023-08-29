badc74e03eba9aff1c88f444fa7a1ee631977f8a
Perseo Enderiz <perseoheroico@gmail.com>
2023-08-28 18:41:34 -0600

    Cambio de etiquetas html a componentes material design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import FormSignUp from './components/FormSignUp';
+import { Container, Typography } from '@mui/material';
 
 function App() {
   return (
-    <>
-      <h1> Formulario </h1>
+    <Container maxWidth="sm" component="Section">
+      <Typography variant="h3"> Formulario </Typography>
       <FormSignUp/>
-    </>
+    </Container>
   );
 }
 
--- src/components/FormSignUp/index.jsx
+++ src/components/FormSignUp/index.jsx
@@ -1,30 +1,26 @@
-import { Button, Switch } from "@mui/material";
+import React from "react";
+import { TextField, Switch, FormControlLabel, Button, FormGroup } from "@mui/material";
 
 function FormSignUp() {
   const formulario = (
     <form>
-      <label htmlFor="nombre">Nombre:</label>
-      <input type="text" id="nombre" name="nombre" />
+      <TextField id="nombre" label="Nombre" fullWidth margin="normal" />
+      <TextField id="apellido" label="Apellido" fullWidth margin="normal" />
+      <TextField id="correo" label="Correo electrónico" fullWidth margin="normal" />
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch defaultChecked/>}
+          label="Novedades"
+        />
+        <FormControlLabel
+          control={<Switch defaultChecked/>}
+          label="Promociones"
+        />
+      </FormGroup>
       <br />
-      <br />
-      <label htmlFor="apellido">Apellido:</label>
-      <input type="text" id="apellido" name="apellido" />
-      <br />
-      <br />
-      <label htmlFor="correo">Correo electrónico:</label>
-      <input type="email" id="correo" name="correo" />
-      <br />
-      <br />
-      <Switch id="novedades" name="novedades" />
-      <label htmlFor="novedades">Novedades</label>
-      <br />
-      <br />
-      <Switch type="checkbox" id="promociones" name="promociones" />
-      <label htmlFor="promociones">Promociones</label>
-      <br />
-      <br />
-      <Button variant="contained">Registrarse</Button>
-
+      <Button variant="contained" color="primary">
+        Registrarse
+      </Button>
     </form>
   );
 
